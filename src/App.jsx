d0ed756e1b62888774ebfc101c36c7556f0a866a
Madhankannan02 <madhankannan29@gmail.com>
2025-12-25 20:01:58 +0530

    Started adding the mask

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="min-h-screen w-full flex justify-center items-center overflow-hidden bg-[#222222]">
+
+        <div className="og">
+
+          <p className="w-150
+          text-2xl
+          text-[rgba(255,255,255,0.4)]
+          font-semibold
+          leading-snug
+          ">
+            Writing <span className="
+            text-[#ddfc3e]
+            "> 
+            beautiful code
+            </span> 
+            means thinking Like an artist and debugging like a detective. Every function is a story, every variable a character. Master your craft through practice, patience, and endless curiosity about how things work.
+            </p>
+        </div>
+
+        <div className="
+        mask
+        min-h-screen
+        w-full
+        flex
+        justify-center
+        items-center
+        overflow-hidden
+        absolute
+        bg-[#ddfc3e]
+        mask-[url('./Images/Yellow_Circle.png')]
+        [mask-size:theme(size.mask-regular)]
+        mask-no-repeat
+        pointer-events-none
+        ">
+          <p className="w-150
+          text-2xl
+          text-black
+          font-semibold
+          leading-snug
+          ">Building 
+          <span className="
+          text-[#ff0000]
+          ">great software</span> requires seang beyond syntax into architecture and design. Test early, refactor often, document clearly. Success comes from collaboration, continuous learning, and caring deeply about user experience.</p>
+        </div>
+
+    </main>
   )
 }
 
